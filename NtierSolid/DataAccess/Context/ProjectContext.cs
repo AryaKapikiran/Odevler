816960b390064066c8f8996f5ccb5c29b16c681a
AryaKapikiran <aryakapikiran96@gmail.com>
2022-04-20 13:22:15 +0300

    katmanlar

---
+++ NtierSolid/DataAccess/Context/ProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class ProjectContext:DbContext
+    {
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server=LAPTOP-OTAOT39G;Database=NtierSolid;Trusted_Connection=True;";
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Category { get; set; }
+
+        DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ NtierSolid/DataAccess/Entity/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entity
+{
+    public  class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        List<Product> Products { get; set; }
+
+        List<Category> Categories { get; set; }
+    }
+}
