b5c1234c23d6cfdbc0340b68a553827e4112b7c9
AryaKapikiran <aryakapikiran96@gmail.com>
2022-05-15 01:10:38 +0300

    listeleme-sayıgösterme-detay

---
+++ MVC_Northwind/MVC_Northwind/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using MVC_Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthWindEntities db = new NorthWindEntities();
+
+        // GET: Home
+
+        public static List<Customer> customers = new List<Customer>();
+        public static List<Order> orders = new List<Order>();
+
+        public ActionResult Index()
+        {
+            customers = db.Customers.OrderBy(x=>x.CustomerID).ToList();
+            orders =db.Orders.OrderBy(x=>x.OrderID).ToList();
+            List<Employee> employees = db.Employees.OrderBy(x=>x.EmployeeID).ToList();
+            List<Product> products = db.Products.OrderBy(x=>x.ProductID).ToList();
+            TempData["Customers"]= customers.ToList();
+            TempData["Orders"]= orders.ToList();
+            TempData["Employees"] = employees.ToList();
+            TempData["Products"] = products.ToList();
+            TempData["ProductNumber"] = products.Count().ToString();
+            TempData["EmployeeNumber"] = employees.Count().ToString();
+            TempData["CustomersNumber"] = customers.Count().ToString();
+            TempData["OrdersNumber"]= orders.Count().ToString();
+            TempData["SonSiparişler"] = orders.OrderByDescending(x=>x.OrderDate).Take(30).ToList();
+            
+            TempData.Keep();
+            return View();
+        }
+
+        
+        public ActionResult CustomerDetaylari(string id)
+        {
+            var customerdetay = customers.Find(x => x.CustomerID == id);
+            
+            return View(customerdetay);
+        }
+
+        public ActionResult OrderDetaylari(int id)
+        {
+            var orderdetay = orders.Find(x => x.OrderID == id);
+
+            return View(orderdetay);
+        }
+    }
+}
